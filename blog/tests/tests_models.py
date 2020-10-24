a4fbc8fc930f4df9c2cb75cf0bf610ca42b3bb8c
Dima <dimalgpf@ukr.net>
2020-10-24 05:12:02 +0300

    Added views tests for blog

--- blog/tests/tests_models.py
+++ blog/tests/tests_models.py
@@ -2,16 +2,18 @@ from django.test import TestCase
 from blog.models import Post
 from django.contrib.auth import get_user_model
 
-class BlogTestClass(TestCase):
-    @classmethod
-    def setUpTestData(self):
-        user = get_user_model()
-        author_user = user.objects.create_user(first_name = 'author',username = 'authoruser',password = '123')
-        self.testpost = Post.objects.create(
-            title = 'Post 1', author = author_user,content = '244466666', 
-            status = 0)
-        self.testpost.save()
+
+#class BlogTestClass(TestCase):
+    # @classmethod
+    # def setUpTestData(self):
+    #     user = get_user_model()
     
-    def test_created_properly(self):
-        self.assertTrue(isinstance(self.testpost, Post))
-        self.assertEqual(self.testpost.__str__(), f"{self.testpost.title} ({self.testpost.created_on})")
+    #     admin = user.objects.create_superuser('superuser@mail.com', 'supersuper')
+    #     self.testpost = Post.objects.create(
+    #         title = 'info',slug = 'info', author = admin,content='244466666', 
+    #         status = 0, title_image= )
+    #     self.testpost.save()
+    
+    # def test_created_properly(self):
+    #     self.assertTrue(isinstance(self.testpost, Post))
+    #     self.assertEqual(self.testpost.__str__(), f"{self.testpost.title} ({self.testpost.created_on})")
