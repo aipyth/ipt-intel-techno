523957d2ddfbfa047568732db23a534820aeb4b5
Ivan <ipython10@gmail.com>
2020-10-31 17:38:51 +0200

    competitor log in/out

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CompetitorDetail
+from .views import CompetitorDetail, LoginView, LogoutView
 
 
 urlpatterns = [
     path('competitor/<pk>/', CompetitorDetail.as_view(),
          name='competitor_detail'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
